18baa0d865294f82316ab5d5a24639c1ab8c4168
Petrik Lajos Szg <info@petrik.hu>
2018-09-21 15:34:32 +0200

    Első rész kész

--- Git3/Program.cs
+++ Git3/Program.cs
@@ -10,6 +10,24 @@ namespace Git3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Kérem adjon meg 10 számot");
+            int[]t= new int[10];
+            int legnagyobb = 0;
+            for (int i = 0; i <t.Length; i++)
+            {
+                t[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Az {0}. szám {1}",i+1,t[i]);
+                if (legnagyobb<t[i])
+                {
+                    legnagyobb = t[i];
+                }
+
+            }
+            Console.WriteLine("Legnagyobb:"+legnagyobb);
+            Console.ReadKey();
+            
+
         }
+
     }
 }
